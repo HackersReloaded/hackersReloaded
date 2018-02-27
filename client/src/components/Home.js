70c1a14468ffe0ffc3d251305927d8eeea226438
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-02-27 12:09:48 -0600

    added a new background logo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,22 +1,40 @@
 import React from "react";
 
  class Home extends React.Component {
+   constructor(props) {
+     super();
+     this.state = {
+         id: props.initalId,
+         status:0
+     };
+     setTimeout(() => {
+       this.setState({
+         status:3
+       })
+     },3000);
 
+   }
+
+   onSpecialDiscount() {
+     this.setState({
+       id: this.state.id +10
+     });
+   }
   render() {
 
     return (
       <div>
          <h2 id="m-tools">{this.props.tools}</h2>
          <img id="m-image" src="Tools.jpg"/>
-         <p id="headertest">Id:{this.props.id}</p>
+         <p id="headertest">{this.state.id}</p>
+         <p>Status: {this.state.status}</p>
          <br/>
-         <div className="col-xs-20 col -xs-offset-5">
            <p>{this.props.quantity}</p>
-           </div>
+         <button onClick={() => this.onSpecialDiscount()} className="btn btn-primary">Special Discount!</button>
          <p>{this.props.description}</p>
+         <button onClick={this.props.greet} className="btn btn-primary">Greeting!</button>
       </div>
     );
   }
 };
 export default Home;
-
